f03c5b309f57d51f1c2f088a03f7c1297991c353
yasuomaidana <yasuomaidana@gmail.com>
2025-01-03 16:46:02 -0600

    Implementing extract by type mut and inmutable for a Product's vector

--- src/product.rs
+++ src/product.rs
@@ -22,3 +22,135 @@ impl Product {
         );
     }
 }
+
+pub fn extract_by_type_mut(products: &mut Vec<&mut Product>, product_type: &str) -> Option<Vec<Product>> {
+    let mut to_return = Vec::new();
+    products.retain(|product| {
+        if product.product_type == product_type {
+            to_return.push((*product).clone());
+            false
+        } else {
+            true
+        }
+    });
+    if to_return.is_empty() {
+        None
+    } else {
+        Some(to_return)
+    }
+}
+
+pub fn extract_by_type(products: &mut Vec<Product>, product_type: &str) -> Option<Vec<Product>> {
+    let mut to_return = Vec::new();
+    products.retain(|product| {
+        if product.product_type == product_type {
+            to_return.push(product.clone());
+            false
+        } else {
+            true
+        }
+    });
+    if to_return.is_empty() {
+        None
+    } else {
+        Some(to_return)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    
+    
+    #[test]
+    fn test_extract_by_type() {
+        let mut products = vec![
+            Product {
+                date: "2021-01-01".to_owned(),
+                product: "Cerveza".to_owned(),
+                product_type: "Bebida".to_owned(),
+                place: "Bar".to_owned(),
+                price: Some(2.0),
+            },
+            Product {
+                date: "2021-01-01".to_owned(),
+                product: "Cerveza".to_owned(),
+                product_type: "Bebida".to_owned(),
+                place: "Bar".to_owned(),
+                price: Some(2.0),
+            },
+            Product {
+                date: "2021-01-01".to_owned(),
+                product: "Cerveza".to_owned(),
+                product_type: "Bebida".to_owned(),
+                place: "Bar".to_owned(),
+                price: Some(2.0),
+            },
+            Product {
+                date: "2021-01-01".to_owned(),
+                product: "Cerveza".to_owned(),
+                product_type: "Bebida".to_owned(),
+                place: "Bar".to_owned(),
+                price: Some(2.0),
+            },
+            Product {
+                date: "2021-01-01".to_owned(),
+                product: "Cerveza".to_owned(),
+                product_type: "Bebida".to_owned(),
+                place: "Bar".to_owned(),
+                price: Some(2.0),
+            },
+        ];
+        let beverages = extract_by_type(&mut products, "Bebida");
+        
+        assert_eq!(beverages.unwrap().len(), 5);
+        assert_eq!(products.len(), 0);
+    }
+    
+    #[test]
+    fn test_extract_by_type_mut(){
+        let mut products = vec![
+            Product {
+                date: "2021-01-01".to_owned(),
+                product: "Cerveza".to_owned(),
+                product_type: "Bebida".to_owned(),
+                place: "Bar".to_owned(),
+                price: Some(2.0),
+            },
+            Product {
+                date: "2021-01-01".to_owned(),
+                product: "Cerveza".to_owned(),
+                product_type: "Bebida".to_owned(),
+                place: "Bar".to_owned(),
+                price: Some(2.0),
+            },
+            Product {
+                date: "2021-01-01".to_owned(),
+                product: "Cerveza".to_owned(),
+                product_type: "Bebida".to_owned(),
+                place: "Bar".to_owned(),
+                price: Some(2.0),
+            },
+            Product {
+                date: "2021-01-01".to_owned(),
+                product: "Cerveza".to_owned(),
+                product_type: "Bebida".to_owned(),
+                place: "Bar".to_owned(),
+                price: Some(2.0),
+            },
+            Product {
+                date: "2021-01-01".to_owned(),
+                product: "IVA".to_owned(),
+                product_type: "Impuestos".to_owned(),
+                place: "Bar".to_owned(),
+                price: Some(2.0),
+            },
+        ];
+        let mut products = products.iter_mut().collect::<Vec<_>>();
+        let beverages = extract_by_type_mut(&mut products, "Bebida");
+        
+        assert_eq!(beverages.unwrap().len(), 4);
+        assert_eq!(products.len(), 1);
+    }
+}
